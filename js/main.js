b57c1bc6a0795afc9a88daca7fdd4744257ed852
K-Violet <eyhc1212@naver.com>
2022-01-02 15:55:34 +0900

    copyright수정

--- js/main.js
+++ js/main.js
@@ -1,86 +1,96 @@
-$(function(){
-    var h = 0;
-    var n = 0;
-    var t = 0;
+$(function () {
+  var h = 0;
+  var n = 0;
+  var t = 0;
 
-    setTimeout(function(){
-        $("html, body").animate({scrollTop : 0}, 800, function(){
-            $(".controller li").eq(0).find("a").addClass("on");
-        });
-    },150);
-
-    $(window).resize(function(){
-        h = $(this).height();
-        $("#page1 .section").css({height : h});
-        distance = n * h;
-        $("html, body").animate({scrollTop : distance}, 0);
+  setTimeout(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 800, function () {
+      $(".controller li").eq(0).find("a").addClass("on");
     });
-    $(window).trigger("resize");
+  }, 150);
 
-    // MENU CLICK
-    $(".controller li a").click(function(e){
-        e.preventDefault();
-        n = $(this).parent().index();
-        distance = n * h;
-
-        $("html, bady").stop().animate({scrollTop : distance}, 800, function(){
-            $(".controller li").find("a").removeClass("on");
-            $(".controller li").eq(n).find("a").addClass("on");
-        });
+  $(window).resize(function () {
+    h = $(this).height();
+    $("#page1 .section").css({
+      height: h
     });
+    distance = n * h;
+    $("html, body").animate({
+      scrollTop: distance
+    }, 0);
+  });
+  $(window).trigger("resize");
 
-    // SCROLL
-    $(window).scroll(function(){
-        t = $(window).scrollTop();
+  // MENU CLICK
+  $(".controller li a").click(function (e) {
+    e.preventDefault();
+    n = $(this).parent().index();
+    distance = n * h;
 
-        if(t <= $("#page2").offset().top){
-            $("#page2 .title").addClass("active");
-        }else if(t <= $("#page3").offset().top){
-            $("#page3 .title").addClass("active");
-        }else if(t <= $("#page4").offset().top){
-            $("#page4 .title").addClass("active");
-        }else{
-            $("#page5 .title").addClass("active");
-        }
+    $("html, bady").stop().animate({
+      scrollTop: distance
+    }, 800, function () {
+      $(".controller li").find("a").removeClass("on");
+      $(".controller li").eq(n).find("a").addClass("on");
     });
+  });
 
-    // MOUSEWHEEL
-    $(".section").mousewheel(function(e, delta){
-        if($("html, bady").is(":animated")){
-            return false;
-        }
+  // SCROLL
+  $(window).scroll(function () {
+    t = $(window).scrollTop();
 
-        if(delta > 0){
-            if(n > 0){
-                n--;
-            }
-        }else{
-            if(n < 5){
-                n++;
-            }
-        }
+    if (t <= $("#page2").offset().top) {
+      $("#page2 .title").addClass("active");
+    } else if (t <= $("#page3").offset().top) {
+      $("#page3 .title").addClass("active");
+    } else if (t <= $("#page4").offset().top) {
+      $("#page4 .title").addClass("active");
+    } else {
+      $("#page5 .title").addClass("active");
+    }
+  });
 
-        distance = n * h;
+  // MOUSEWHEEL
+  $(".section").mousewheel(function (e, delta) {
+    if ($("html, bady").is(":animated")) {
+      return false;
+    }
 
-        $("html, bady").stop().animate({scrollTop : distance}, 800, function(){
-            $(".controller li").find("a").removeClass("on");
-            $(".controller li").eq(n).find("a").addClass("on");
-        });
+    if (delta > 0) {
+      if (n > 0) {
+        n--;
+      }
+    } else {
+      if (n < 5) {
+        n++;
+      }
+    }
+
+    distance = n * h;
+
+    $("html, bady").stop().animate({
+      scrollTop: distance
+    }, 800, function () {
+      $(".controller li").find("a").removeClass("on");
+      $(".controller li").eq(n).find("a").addClass("on");
     });
+  });
 
-    // SCROLLMAGIC
-    const spyEls = document.querySelectorAll("div.scroll-spy");
-    spyEls.forEach(function(spyEl){
-        new ScrollMagic
-        .Scene({
-            triggerElement : spyEl,
-            triggerHook : 0.5
-        })
-        .setClassToggle(spyEl, "show")
-        .addTo(new ScrollMagic.Controller());
-    });
+  // SCROLLMAGIC
+  const spyEls = document.querySelectorAll("div.scroll-spy");
+  spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+      .Scene({
+        triggerElement: spyEl,
+        triggerHook: 0.5
+      })
+      .setClassToggle(spyEl, "show")
+      .addTo(new ScrollMagic.Controller());
+  });
 
-    // DATE
-    const thisYear = document.querySelector(".this-year");
-    thisYear.textContent = new Date().getFullYear();
+  // DATE
+  const thisYear = document.querySelector(".this-year");
+  thisYear.textContent = new Date().getFullYear();
 });
